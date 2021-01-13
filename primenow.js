483a6094445d8e94b70db00a57c490892d2fdcef
Quentin Gibson <qgibson3@students.kennesaw.edu>
2021-01-13 09:07:58 -0500

    Fix: Correct url pattern in Prime Now
    Prime now button should correctly take you take the item page in the
    internal system. The internal system button has not been tested yet.

--- primenow.js
+++ primenow.js
@@ -1,10 +1,10 @@
-const hrefPattern = 'change_this'
+const hrefPattern = 'https://aftlite-na.amazon.com/inventory/view_inventory_for_asin?asin='
 browser.runtime.onMessage.addListener(message => {
   if (message.command === 'asin') appendButton(message.url)
 })
 browser.runtime.sendMessage({command: 'get'})
 function appendButton(url) {
-  const asin = url.substring(url.indexOf('d') + 2, url.indexOf('?'))
+  const asin = url.substring(url.indexOf('d') + 3, url.indexOf('?'))
   const amazonButton = document.createElement('a')
   amazonButton.text = "Go To Item Page"
   amazonButton.href = `${hrefPattern}${asin}`
